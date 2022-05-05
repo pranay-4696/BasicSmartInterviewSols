a94f61ea8ae5f2f7da543f2272e07e952c17b18b
pranay macharla <96190485+pranay-4696@users.noreply.github.com>
2022-05-05 23:38:09 +0530

    SUM OF NATURAL NUMBERS
    SUM OF NATURAL NUMBERS

---
+++ NaturalNumberSum.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in=new Scanner(System.in);
+
+		int n=in.nextInt();
+
+		int f=0;
+
+		f=((n)*(n+1))/2;
+
+		System.out.print(f);
+    }
+}
