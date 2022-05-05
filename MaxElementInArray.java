9b97c941e0be22ce107dff1fc66ab0a0b8085eb3
pranay macharla <96190485+pranay-4696@users.noreply.github.com>
2022-05-05 23:24:23 +0530

    MAXIMUM ELEMENT IN ARRAY
    MAXIMUM ELEMENT IN ARRAY

--- MaxElementInArray.java
+++ MaxElementInArray.java
@@ -6,7 +6,8 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in=new Scanner (System.in);
         int n=in.nextInt();
